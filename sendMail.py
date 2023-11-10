9e9b42ce279f55351ad79a383b849423242cedca
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2023-11-10 19:18:57 +0300

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -1,20 +1,26 @@
+#This script sends mail to your address
+
+fromaddr = ""
+toaddr = ""
+mypass = ""
+subject = ""
+body = ""
+server = "smtp.mail.ru" # I use mail.ru to send notifications to my address
+port = 465
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
- 
-fromaddr = ""
-toaddr = ""
-mypass = "" #SPECIAL PASSWORD FOR THIS APPLICATION
- 
+
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "Subject"
+msg['Subject'] = subject
  
-body = "Test message"
+
 msg.attach(MIMEText(body, 'plain'))
  
-server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+server = smtplib.SMTP_SSL(server, port)
 server.login(fromaddr, mypass)
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
